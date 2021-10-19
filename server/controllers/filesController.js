42d46f7080f50853419922ad3e7d4d125991e671
Vladyslav <v.polianychka@gmail.com>
2021-10-19 21:37:33 +0300

    Add download multiple file functionality

--- server/controllers/filesController.js
+++ server/controllers/filesController.js
@@ -1,6 +1,8 @@
 import path from 'path'
 import fs from 'fs'
 import File from '../models/fileModel.js'
+import archiver from 'archiver'
+import mime from 'mime'
 
 class FilesController {
     async get(req, res) {
@@ -41,9 +43,26 @@ class FilesController {
             if(paths.length === 1) {
                 res.download(paths[0])
             } else {
+                const archivePath = `${path.resolve('uploads')}/archive.zip`
+                const output = fs.createWriteStream(archivePath)
+                const archive = archiver('zip', {
+                    zlib: { level: 9 }
+                })
+                
+                archive.pipe(output)
 
+                paths.forEach((p, idx) => {
+                    archive.append(fs.createReadStream(p), { name: fileNames[idx] })
+                })
+                
+                archive.finalize()
+
+                output.on('close', () => {
+                    res.download(archivePath, () => {
+                        fs.unlink(archivePath, (e) => e && console.log(e))
+                    })
+                })
             }
-
         } catch(e) {
             console.log(e.message)
             res.status(404).send('file not found')
